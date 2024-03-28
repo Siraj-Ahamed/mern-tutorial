0462ad895208178c20c23024948a0068b98a4127
Siraj-Ahamed <ahamedsiraj770@gmail.com>
2024-03-28 17:39:33 +0530

    server created

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
 const express = require("express");
+// express app
+const app = express();
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome" });
+});
+
+// lisren for requests
+app.listen(process.env.PORT, () => {
+    console.log(`listening on port ${process.env.PORT}`);
+});
