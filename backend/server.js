a8395894c8d7c6b980fa11d9ce53b3528babebdb
Siraj-Ahamed <ahamedsiraj770@gmail.com>
2024-03-28 23:06:56 +0530

    connected to db

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,34 @@ const express = require("express");
 const app = express();
 
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const workoutRoutes = require("./routes/workouts");
+
 dotenv.config();
 
+// middlewares
+app.use(express.json());
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    next();
+});
+
+// routes
 app.get("/", (req, res) => {
     res.json({ message: "Welcome" });
 });
 
-// lisren for requests
-app.listen(process.env.PORT, () => {
-    console.log(`listening on port ${process.env.PORT}`);
-});
+app.use("/api/workouts", workoutRoutes);
+
+// connect to db
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        // lisren for requests
+        app.listen(process.env.PORT, () => {
+            console.log(`connectes to db & listening on port ${process.env.PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
