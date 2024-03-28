c5a167dcc8d9982b8c8e3ade0b66e385cb4b0fc9
Siraj-Ahamed <ahamedsiraj770@gmail.com>
2024-03-28 23:46:43 +0530

    creating model

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const Workout = require("../models/workoutModel");
 
 // GET all workouts
 router.get("/", (req, res) => {
@@ -12,7 +13,16 @@ router.get("/:id", (req, res) => {
 });
 
 // POST a new workout
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
+    const { title, reps, load } = req.body;
+
+    try {
+        const workout = await Workout.create({ title, reps, load });
+        res.status(201).json(workout);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+
     res.json({ message: "Post a new workout" });
 });
 
