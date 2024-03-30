f0e3fbb178231a9cf4fea802a1477017648cc5f8
Siraj-Ahamed <ahamedsiraj770@gmail.com>
2024-03-30 14:46:06 +0530

    fetching data from backend to frontend

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import WorkoutDetails from "../components/WorkoutDetails";
+
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+    const [workouts, setWorkouts] = useState(null);
+    useEffect(() => {
+        const fetchWorkouts = async () => {
+            const response = await fetch("/api/workouts");
+            const json = await response.json();
+            console.log("response", response);
+            console.log("json", json);
+            if (response.ok) {
+            }
+            setWorkouts(json);
+        };
+        fetchWorkouts();
+    }, []);
+    return (
+        <div>
+            <div className="workouts">
+                {workouts &&
+                    workouts.map((workout) => (
+                        // <p key={workout._id}>{workout.title}</p>
+                        <WorkoutDetails key={workout._id} workout={workout}/>
+                    ))}
+            </div>
+        </div>
+    );
+};
 
-export default Home
+export default Home;
